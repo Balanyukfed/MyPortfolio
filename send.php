ba2ae8bcce91876fc058b4d8ddb918238c4f0cf8
NikitaBalanyuk <55661274+Balanyukfed@users.noreply.github.com>
2022-06-27 16:36:37 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,44 +1,58 @@
 <?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$title = $_POST['title'];
-	$comment = $_POST['comment'];
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $title = $_POST['title'];
+// $comment = $_POST['comment'];
 
 
-	$name = htmlspecialchars($name);
-	$email = htmlspecialchars($email);
-	$title = htmlspecialchars($title);
-	$comment = htmlspecialchars($comment);
-	
-	$name = urldecode($name);
-	$email = urldecode($email);
-	$title = urldecode($title);
-	$comment = urldecode($comment);
+// $name = htmlspecialchars($name);
+// $email = htmlspecialchars($email);
+// $title = htmlspecialchars($title);
+// $comment = htmlspecialchars($comment);
 
-	$name = trim($name);
-	$email = trim($email);
-	$title = trim($title);
-	$comment = trim($comment);
+// $name = urldecode($name);
+// $email = urldecode($email);
+// $title = urldecode($title);
+// $comment = urldecode($comment);
 
-	if (mail("nekitgraviton2002@gmail.com", 
-			 "Новое письмо с сайта",
-			 "Имя: ".$name."\n".
-			 "Email: ".$email."\n".
-			 "Тема: ".$title."\n".
-			 "Комментарий: ".$comment."\n",
-			"Content-type:text/plain; charset = UTF-8")
-			
-			) {
-		echo ('Письмо успешно отправлено!');
-	}
-	else{
-		echo ('Есть ошибки! Проверьте данные...');
-	}
+// $name = trim($name);
+// $email = trim($email);
+// $title = trim($title);
+// $comment = trim($comment);
 
-// 	$send = mail("nekitgraviton2002@gmail.com",
-// 				 $name, $email, $title, $comment, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+// if (mail("nekitgraviton2002@gmail.com", 
+// 	"Новое письмо с сайта",
+// 	"Имя: ".$name."\n".
+// 	"Email: ".$email."\n".
+// 	"Тема: ".$title."\n".
+// 	"Комментарий: ".$comment."\n",
+// 	"Content-type:text/plain; charset = UTF-8")
 
-// 	if ($send == 'true') {echo "Сообщение отправлено";}
-// // Если письмо не ушло — выводим сообщение об ошибке
-// else {echo "Ой, что-то пошло не так";}
+// ) {
+// 	echo ('Письмо успешно отправлено!');
+// }
+// else{
+// 	echo ('Есть ошибки! Проверьте данные...');
+// }
+
+ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['title']) and !empty($_POST['comment'])){
+      $name = trim(strip_tags($_POST['name']));
+      $email = trim(strip_tags($_POST['email']));
+      $title = trim(strip_tags($_POST['title']));
+      $comment = trim(strip_tags($_POST['comment']));
+ 
+      mail('nekitgraviton2002@gmail.com', 'Письмо с адрес_вашего_сайта', 
+      'Вам написал: '.$name.'<br />Его номер: '.$email.'<br />Его почта: '.$title.'<br />
+      Его сообщение: '.$comment,"Content-type:text/html;charset=UTF-8");
+ 
+      echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
+      ближайшее время<Br> $back";
+ 
+      exit;
+   } 
+   else {
+      echo "Для отправки сообщения заполните все поля!";
+      exit;
+   }
 ?>
