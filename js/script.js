e2a2b1e6ed26195895d6af984dc197a8cb8531f9
NikitaBalanyuk <55661274+Balanyukfed@users.noreply.github.com>
2022-06-27 22:07:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -219,7 +219,7 @@ form.addEventListener('submit', function(evt){
         console.log(request.response);
         alert('Ваша заявка успешно отправлена!!!');
     });
-    request.open('POST', 'https://nekitg2i.beget.tech/send.php', true);
+    request.open('POST', 'http://nekitg2i.beget.tech/send.php', true);
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     request.send('name=' + encodeURIComponent(formData.name) + '&email=' +  encodeURIComponent(formData.email) + '&title=' +  encodeURIComponent(formData.title) + '&comment=' +  encodeURIComponent(formData.comment));
 });
